2fdd86ca2288b7134c2be65aea38213e9ed36ae5
sua-h <suaah.96@gmail.com>
2022-04-02 18:18:17 +0900

    220402 프로젝트 수정

---
+++ src/main/java/com/example/CyProject/home/HomeController.java
@@ -0,0 +1,17 @@
+package com.example.CyProject.home;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/home")
+public class HomeController {
+
+    @GetMapping
+    public String home() {
+        return "home/index";
+    }
+}
