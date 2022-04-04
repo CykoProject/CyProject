8dd7815a5ca4e1b68ae95bb4063b3b5b1e64d4ce
y7200 <kied1594@naver.com>
2022-04-04 18:17:45 +0900

    2022-04-04 미니홈피 Diary 작업 중

--- src/main/java/com/example/CyProject/home/HomeController.java
+++ src/main/java/com/example/CyProject/home/HomeController.java
@@ -1,7 +1,12 @@
 package com.example.CyProject.home;
 
+import com.example.CyProject.home.model.HomeEntity;
+import com.example.CyProject.home.model.diary.DiaryEntity;
+import com.example.CyProject.home.model.diary.DiaryRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -10,13 +15,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/home")
 public class HomeController {
 
+    @Autowired private DiaryRepository diaryRepository;
+
     @GetMapping
     public String home() {
         return "home/index";
     }
 
     @GetMapping("/diary")
-    public String diary() {
+    public String diary(HomeEntity entity, Model model) {
+        model.addAttribute("data", diaryRepository.findByIhostOrderByRdtDesc(entity.getIuser()));
         return "home/diary";
     }
 }
