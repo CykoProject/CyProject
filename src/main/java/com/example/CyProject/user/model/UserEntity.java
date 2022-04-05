bbf53a6d2af90ee7742382f1f8a20cf27c92a24f
yueonsu <kied1594@naver.com>
2022-04-05 16:43:37 +0900

    2022-04-05 시큐리티 설정

---
+++ src/main/java/com/example/CyProject/user/model/UserEntity.java
@@ -0,0 +1,40 @@
+package com.example.CyProject.user.model;
+
+import com.example.CyProject.config.Role;
+import lombok.*;
+
+import javax.persistence.*;
+import java.sql.Date;
+
+@Entity
+@Table(name = "user")
+@Getter
+@Setter
+@ToString
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int iuser;
+
+    private String email;
+    private String nm;
+    private String upw;
+
+    @Column(insertable = false)
+    private String provider;
+
+    @Column(insertable = false)
+    private String img;
+
+    @Column(insertable = false)
+    private int point;
+
+    @Column(insertable = false)
+    private Date rdt;
+
+    private Role role;
+}
