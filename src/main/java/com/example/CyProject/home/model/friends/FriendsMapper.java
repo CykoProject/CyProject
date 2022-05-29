5a3c68635af1e8e0988d4a5d4b8723c32113bc4f
sua-h <suaah.96@gmail.com>
2022-05-29 18:26:10 +0900

    220529 수정

--- src/main/java/com/example/CyProject/home/model/friends/FriendsMapper.java
+++
@@ -1,8 +0,0 @@
-package com.example.CyProject.home.model.friends;
-
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface FriendsMapper {
-    int selFriends(FriendsEntity entity);
-}
