f7ff6f8345ebd744735f6d13a61a2bb6d9129df2
juyoung <terranzz01@gmail.com>
2022-06-01 14:48:15 +0900

    완료

--- src/main/java/com/example/CyProject/home/model/photo/PhotoImgRepository.java
+++ src/main/java/com/example/CyProject/home/model/photo/PhotoImgRepository.java
@@ -7,6 +7,7 @@ import java.util.List;
 
 public interface PhotoImgRepository extends JpaRepository<PhotoImgEntity, Integer> {
     List<PhotoImgEntity> findAllByIphoto(int iphoto);
+    PhotoImgEntity findByIphoto(int iphoto);
 
     @Transactional
     void deleteAllByIphoto(int iphoto);
